915d6f520809f3a7a1892d18d9b096ac7b6bd688
Piotr Adam Strząska <p.strzaska@stud.elka.pw.edu.pl>
2020-06-06 19:36:14 +0200

    Update Level.java

--- src/DynaBlast/Level.java
+++ src/DynaBlast/Level.java
@@ -15,38 +15,42 @@ import java.io.File;
  * @author Michal Mianowski, Piotr Strzaska
  */
 public class Level {
-    /** margin from left and top of the window */
+    /**
+     * margin from left and top of the window
+     */
     public static int margin = 20;
-    /** timer for level in seconds */
+    /**
+     * timer for level in seconds
+     */
     static int timeLeft = 0; //in seconds
     static int timePoints;
-<<<<<<< HEAD
-    /** should the level will be restarted */
+    /**
+     * should the level will be restarted
+     */
     public boolean restart = false;
-    /** should be begin win procedure */
+    /**
+     * should be begin win procedure
+     */
     public boolean win = false;
-=======
-    public boolean restart = false;
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
 
-    /** two dimensions table of @Block containing all level blocks */
+    /**
+     * two dimensions table of @Block containing all level blocks
+     */
     public static Block[][] block;
     /**
      * two dimensions table of chars containing coded data about level plan
      * receive data from external file by @loadLevelFromFile method
-     * */
+     */
     public static char[][] blockList;
-    /** int table with information where will be escape cell */
+    /**
+     * int table with information where will be escape cell
+     */
     public int[] cellOfEscape = new int[2];
-<<<<<<< HEAD
-    /** flag of escape visible stage
+    /**
+     * flag of escape visible stage
      * 0 -not, 1 - ready to destroy, 2 - ready to escape
      */
-    public int escapeVisible = 0;
-=======
     public int escapeVisible = 0; //0 -not, 1 - ready to destroy, 2 - ready to escape
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
-
 
 
     static File LevelSize1 = new File("res/config/Levels/Poziom rozmiar.txt");
@@ -57,14 +61,22 @@ public class Level {
     static File LevelLocation5 = new File("res/config/Levels/Poziom_5.txt");
     static File level;
 
-    /** ArrayList containing all enemies at this level */
+    /**
+     * ArrayList containing all enemies at this level
+     */
     static ArrayList<Enemy> enemies = new ArrayList();
-    /** ArrayList containing all bombs unexploded */
+    /**
+     * ArrayList containing all bombs unexploded
+     */
     static ArrayList<Bomb> bombs = new ArrayList();
-    /** ArrayList containing all present explosions */
+    /**
+     * ArrayList containing all present explosions
+     */
     static ArrayList<Explosion> explosions = new ArrayList();
 
-    /** Creates object of Level */
+    /**
+     * Creates object of Level
+     */
     public Level() {
         Configurations.generateLevel();
         Configurations.loadLevelFromFile(level);
@@ -80,19 +92,25 @@ public class Level {
         for (int y = 0; y < block.length; y++) {
             for (int x = 0; x < block.length; x++) {
                 if (lvl[x][y] == Tile.empty) {
-                } else if (lvl[x][y] == Tile.unbreakable) {
+                }
+                else if (lvl[x][y] == Tile.unbreakable) {
                     block[x][y].id = Tile.unbreakable;
-                } else if (lvl[x][y] == Tile.bars) {
+                }
+                else if (lvl[x][y] == Tile.bars) {
                     block[x][y].id = Tile.bars;
-                } else if (lvl[x][y] == Tile.escape_2) {
+                }
+                else if (lvl[x][y] == Tile.escape_2) {
                     System.out.println("wczytano drabinę");
                     cellOfEscape[0] = x;
                     cellOfEscape[1] = y;
-                } else if (lvl[x][y] == Tile.guard) {
+                }
+                else if (lvl[x][y] == Tile.guard) {
                     enemies.add(new Enemy(Tile.guard, margin + x * Tile.tileSize, margin + y * Tile.tileSize));
-                } else if (lvl[x][y] == Tile.army_man) {
+                }
+                else if (lvl[x][y] == Tile.army_man) {
                     enemies.add(new Enemy(Tile.army_man, margin + x * Tile.tileSize, margin + y * Tile.tileSize));
-                } else if (lvl[x][y] == Tile.swat) {
+                }
+                else if (lvl[x][y] == Tile.swat) {
                     enemies.add(new Enemy(Tile.swat, margin + x * Tile.tileSize, margin + y * Tile.tileSize));
                 }
             }
@@ -100,7 +118,9 @@ public class Level {
     }
 
     public void tick() {
-        if(restart){ restart();}
+        if (restart) {
+            restart();
+        }
         CollisionChecker.isContactCharacterEnemy(enemies, Game.character);
 
         bombs.removeIf(bomb -> bomb.stage < -1);
@@ -113,44 +133,39 @@ public class Level {
         escapeVisibility();
     }
 
-<<<<<<< HEAD
     /**
      * adding bomb to list of bombs at map
+     *
      * @param cell in which cell bomb should be placed
      */
-=======
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
-    public void addBomb(int[] cell){
+    public void addBomb(int[] cell) {
         bombs.add(new Bomb(cell));
     }
 
-    /** render each block in two dimensions block table
+    /**
+     * render each block in two dimensions block table
      *
      * @param g Graphic to which render images
      */
     public void render(Graphics g) {
         for (int y = 0; y < block.length; y++) {
-            for (int x = 0; x < block.length; x++) {
-                block[x][y].render(g);
+            for (Block[] blocks : block) {
+                blocks[y].render(g);
             }
         }
-        if(escapeVisible == 1){
+        if (escapeVisible == 1) {
             block[cellOfEscape[0]][cellOfEscape[1]].render(g, Tile.escape_1);
-        }
-        else if(escapeVisible == 2){
+        } else if (escapeVisible == 2) {
             block[cellOfEscape[0]][cellOfEscape[1]].render(g, Tile.escape_2);
         }
     }
 
-<<<<<<< HEAD
     /**
      * restart procedure
      * cleaning enemies, bombs
      * and reloading level
      */
-=======
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
-    public void restart(){
+    public void restart() {
         restart = false;
         escapeVisible = 0;
         bombs.clear();
@@ -160,25 +175,21 @@ public class Level {
         loadLevel(blockList);
     }
 
-<<<<<<< HEAD
-    /**control visibility of escape place image */
-=======
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
+    /**
+     * control visibility of escape place image
+     */
+
     private void escapeVisibility() {
-        if((enemies.size() == 0)&&(escapeVisible == 0)){
+        if ((enemies.size() == 0) && (escapeVisible == 0)) {
             escapeVisible = 1;
         }
     }
-<<<<<<< HEAD
 
-    /** procedure after winning the level*/
+    /**
+     * procedure after winning the level
+     */
     public void win() {
-        if (!win) {
-            System.out.println("tadaaa");
-            win = true;
-        }
-=======
-     public void win() {
+        Thread.interrupted();
         int points1 = Game.character.getScore();
         int points2 = Level.timeLeft*timePoints;
         int points = points1 + points2;
@@ -208,8 +219,5 @@ public class Level {
             Menu.game.start();
             Character.score = points;
         }
->>>>>>> 12b87d77863e8f3ea5013a8825599a821b710802
     }
-
-
 }
